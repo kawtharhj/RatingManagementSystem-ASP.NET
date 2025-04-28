a840896bf56ddb09160cb4c2655dd1bf2ba48545
kawthar <kawthar.hijazi.97@gmail.com>
2025-04-28 23:45:34 +0300

    Initial commit

--- obj/Debug/net8.0/RatingManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/RatingManagementSystem.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("RatingManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d296dd4d6d8d8b712d332cbf86d9dac992e4d47f")]
 [assembly: System.Reflection.AssemblyProductAttribute("RatingManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RatingManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
